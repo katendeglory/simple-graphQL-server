0425d54bd9702590a7e628115768efe84cb31d49
Glory Katende <katendeglory@gmail.com>
2020-02-10 18:16:58 +0200

    Yet another commit

--- server/resolvers/comment.js
+++ server/resolvers/comment.js
@@ -25,9 +25,21 @@ module.exports = {
       } catch (error) {
         throw error;
       }
+    },
+    deleteComment: async (_, { postID, commentID }, context) => {
+      const decoded = utils.verifyToken(context);
+      if (!decoded) throw AuthenticationError('The Token is invalid');
+
+      const postToUpdate = await Post.findById(postID);
+      if (!postToUpdate) throw Error('Not Post was returned');
+
+      const commentIndex = postToUpdate.comments.findIndex(c => c.id === commentID);
+      if (commentIndex === -1) throw Error('Not Comment was returned');
+
+      if (decoded.username !== postToUpdate.comments[commentIndex].username) throw new AuthenticationError('Action Not Allowed');
+      postToUpdate.comments.splice(commentIndex, 1);
+
+      return await postToUpdate.save();
     }
   }
 }
-
-
-//in index
